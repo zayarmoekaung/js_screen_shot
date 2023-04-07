75bf8ae9174138911f280f2e59d3bb003b2386fd
Zayar MoeKaung <zayarmoekaung0@gmail.com>
2023-04-07 13:10:14 +0630

    util2

--- dist/main_v4.js
+++ dist/main_v4.js
@@ -161,7 +161,7 @@ b_body.innerHTML = document.body.innerHTML;
             <title>Screen Shot</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/zayarmoekaung/js_screen_shot/dist/screenshot.css">
             <script src="https://cdn.jsdelivr.net/gh/zayarmoekaung/js_screen_shot/dist/FileSaver.js"></script>
-            <script src="https://cdn.jsdelivr.net/gh/zayarmoekaung/js_screen_shot/dist/util_v2.js"></script>
+            <script src="https://cdn.jsdelivr.net/gh/zayarmoekaung/js_screen_shot/dist/util2.js"></script>
         </head>
         <body>
             <div>
